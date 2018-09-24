d7266d30e6246410e1cb15b29817df333d5a5f63
Noel <user@bitcoin.com>
2018-09-24 22:47:40 +0100

    wrote base58 encode

--- litcoin/base58.py
+++ litcoin/base58.py
@@ -5,10 +5,23 @@ import binascii
 BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 def base58_encode(data):
-    bigint = int('0x0' + binascii.hexlify(data).decode('utf8'), 16)
+    enc = ''
 
-    # TODO...
+    if len(data) == 0:
+        return enc
     
+    n = int(data.hex(), 16)
+    
+    while n != 0:
+        r = n % 58
+        n = n // 58
+        enc = BASE58_ALPHABET[r] + enc
+    
+    for b in data:
+        if b == 0x00:
+            enc = '1' + enc
+
+    return enc
 
 def base58_decode(data):
     pass
--- litcoin/tests/test_base58.py
+++ litcoin/tests/test_base58.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
+from litcoin.base58 import base58_encode
+
 def test_base58_encode():
-    assert True
+    actual = base58_encode(b'')
+    expected = ''
+    assert(actual == expected)
+
+    actual = base58_encode(bytes.fromhex('3a'))
+    expected = '21'
+    assert(actual == expected)
+
+    actual = base58_encode(bytes.fromhex('0001'))
+    expected = '12'
+    assert(actual == expected)
+
+    actual = base58_encode(b'litcoin')
+    expected = '57HQmfS6vq'
+    assert(actual == expected)
+
+
+def test_base58_decode():
+    pass
