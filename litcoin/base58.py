162dfa1baa495622e326f594b70245ef5379df4b
Noel <user@bitcoin.com>
2018-09-27 11:00:23 +0100

    Base58 code complete

--- litcoin/base58.py
+++ litcoin/base58.py
@@ -20,8 +20,30 @@ def base58_encode(data):
     for b in data:
         if b == 0x00:
             enc = '1' + enc
+        else:
+            break
 
     return enc
 
 def base58_decode(data):
-    pass
+    dec = bytes()
+    n = 0
+    p = 0
+
+    for digit in reversed(data):
+        n += BASE58_ALPHABET.index(digit) * (58 ** p)
+        p += 1
+
+    while n != 0:
+        r = n % 256
+        n = n // 256
+        dec = bytes([r]) + dec
+
+    for c in data:
+        if c == '1':
+            dec = bytes([0x00]) + dec
+        else:
+            break
+
+    return dec
+
--- litcoin/tests/test_base58.py
+++ litcoin/tests/test_base58.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 
-from litcoin.base58 import base58_encode
+from litcoin.base58 import base58_encode, base58_decode
+
+TEST_CASES = [
+    {'hex': '', 'b58': ''},
+    {'hex': '00', 'b58': '1'},
+    {'hex': '01', 'b58': '2'},
+    {'hex': '0000', 'b58': '11'},
+    {'hex': '000100', 'b58': '15R'},
+    {'hex': '00010001', 'b58': '1LUx'},
+    {'hex': 'ff', 'b58': '5Q'},
+    {'hex': 'ffff', 'b58': 'LUv'},
+    {'hex': '0102030405060708090a0b0c0d0e0f', 'b58': '2drXXUifSrRnXLGbXg8E'}
+]
 
 def test_base58_encode():
-    actual = base58_encode(b'')
-    expected = ''
-    assert(actual == expected)
-
-    actual = base58_encode(bytes.fromhex('3a'))
-    expected = '21'
-    assert(actual == expected)
-
-    actual = base58_encode(bytes.fromhex('0001'))
-    expected = '12'
-    assert(actual == expected)
-
-    actual = base58_encode(b'litcoin')
-    expected = '57HQmfS6vq'
-    assert(actual == expected)
+    for test_case in TEST_CASES:
+        actual = base58_encode(bytes.fromhex(test_case['hex']))
+        expected = test_case['b58']
+        assert actual == expected
 
 
 def test_base58_decode():
-    pass
+    for test_case in TEST_CASES:
+        actual = base58_decode(test_case['b58'])
+        expected = bytes.fromhex(test_case['hex'])
+        assert actual == expected
