5df0d055856e7378ba6740ec3561cabeb05dee01
Noel O'Donnell <odonnell.noel@gmail.com>
2018-10-11 00:34:26 +0100

    Removed base48 functions in favor of base58check functions

--- litcoin/base58check.py
+++ litcoin/base58check.py
@@ -3,15 +3,49 @@
 from litcoin.hashing import double_sha
 from litcoin.base58 import base58_encode, base58_decode
 
-def base58check_encode(bin_data):
-    checksum = double_sha(bin_data)[0:4]
-    return base58_encode(bin_data + checksum)
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-def base58check_decode(b58_data):
-    bin_data = base58_decode(b58_data)
-    payload = bin_data[0:-4]
+def base58check_encode(data):
+    data = data + double_sha(data)[0:4]
+    n = int(data.hex(), 16)
+    enc = ''
 
-    bundled_checksum = bin_data[-4:]
+    while n != 0:
+        r = n % 58
+        n = n // 58
+        enc = BASE58_ALPHABET[r] + enc
+    
+    for b in data:
+        if b == 0x00:
+            enc = '1' + enc
+            continue
+        break
+
+    return enc
+
+
+def base58check_decode(b58str):
+    n = 0
+    p = 0
+    dec = bytes()
+
+    for digit in reversed(b58str):
+        n += BASE58_ALPHABET.index(digit) * (58 ** p)
+        p += 1
+
+    while n != 0:
+        r = n % 256
+        n = n // 256
+        dec = bytes([r]) + dec
+
+    for c in b58str:
+        if c == '1':
+            dec = bytes([0x00]) + dec
+            continue
+        break
+    
+    payload = dec[0:-4]
+    bundled_checksum = dec[-4:]
     computed_checksum = double_sha(payload)[0:4]
 
     if bundled_checksum != computed_checksum:
