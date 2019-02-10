39bfa65307c85dcfc5e5821e6e2ba4b8f549660e
odonnellnoel <odonnellnoel@github>
2019-02-10 21:56:41 +0000

    Improvements to networks, plus tests

--- litcoin/networks.py
+++ litcoin/networks.py
@@ -4,24 +4,44 @@ from litcoin.binhex import b
 
 NETWORKS = {
     'bitcoin': {
+        'name': 'bitcoin',
         'inventor': 'Satoshi Nakamoto',
         'p2pkh_prefix': b('00'),
         'p2sh_prefix': b('05'),
-        'wif_prefix': b('80')
+        'wif_prefix': b('80'),
+        'seconds_per_block': int(10 * 60),
+        'seconds_between_retargets': int(14 * 24 * 60 * 60),
+        'blocks_between_halving': 210000,
+        'genesis_block': {
+            'time': 1231006505,
+            'nonce': 2083236893
+        }
     },
     'litecoin': {
+        'name': 'litecoin',
         'inventor': 'Charles Lee',
         'p2pkh_prefix': b('30'),
         'p2sh_prefix': b('05'),
-        'wif_prefix': b('b0')
+        'wif_prefix': b('b0'),
+        'seconds_per_block': int(2.5 * 60),
+        'seconds_between_retargets': int(3.5 * 24 * 60 * 60),
+        'blocks_between_halving': 840000,
+        'genesis_block': {
+            'time': 1317972665,
+            'nonce': 2084524493
+        }
     }
 }
 
-
 NETWORK_NAMES = set(NETWORKS.keys())
 
+# reverse lookups
 
 NETWORK_BY_P2PKH_PREFIX = {
-    b('00'): NETWORKS['bitcoin'],
-    b('30'): NETWORKS['litecoin']
+    NETWORKS[k]['p2pkh_prefix']: NETWORKS[k] for k in NETWORKS
 }
+
+"""
+Note that there should be **no** NETWORK_BY_P2SH_PREFIX since
+bitcoin and litecoin have the same prefix (0x05)
+"""
---
+++ litcoin/tests/test_networks.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from litcoin.networks import NETWORKS, NETWORK_NAMES, NETWORK_BY_P2PKH_PREFIX
+import unittest
+
+
+class TestNetworks(unittest.TestCase):
+    def test_NETWORKS(self):
+        assert(set(NETWORKS.keys()) == set(['bitcoin', 'litecoin']))
+
+        for network_name in NETWORKS:
+            network = NETWORKS[network_name]
+            assert(set(network.keys()) == set(['name', 'inventor', 'p2pkh_prefix', 'p2sh_prefix', 'wif_prefix', \
+                'seconds_per_block', 'seconds_between_retargets', 'blocks_between_halving', 'genesis_block'])), \
+                '`network.keys()` should contain correct properties for network {0}'.format(network_name)
+            assert type(network_name) is str, '`network_name` should be of type str for network {0}'.format(network_name)
+            assert type(network) is dict, '`network` should be of type dict for network {0}'.format(network_name)
+            assert type(network['name']) is str, '`network[\'name\']` should be of type str for network {0}'.format(network_name)
+            assert network_name == network['name'], 'network_name should be equal to network[\'name\'] for network {0}'.format(network_name)
+            assert type(network['seconds_per_block']) is int, '`network[\'seconds_per_block\']` should be of type int for network {0}'.format(network_name)
+            assert 0 < network['seconds_per_block'], '`network[\'seconds_per_block\']` should be greater than zero for network {0}'.format(network_name)
+            assert 0 < network['seconds_between_retargets'], '`network[\'seconds_between_retargets\']` should be greater than zero for network {0}'.format(network_name)
+            assert 0 < network['blocks_between_halving'], '`network[\'blocks_between_halving\']` should be greater than zero for network {0}'.format(network_name)
+            
+            genesis_block = network['genesis_block']
+            assert set(genesis_block.keys()) == set(['time', 'nonce']), '`network[\'genesis_block\'].keys()` should contain correct properties for network {0}'.format(network_name)
+            assert 0 < genesis_block['time'], '`genesis_block[\'time\']` should be greater than zero for network {0}'.format(network_name)
+            assert 0 < genesis_block['nonce'], '`genesis_block[\'nonce\']` should be greater than zero for network {0}'.format(network_name)
+
+    
+    def test_NETWORK_NAMES(self):
+        assert NETWORK_NAMES == set(NETWORKS.keys()), 'NETWORK_NAMES should be equal to set(NETWORKS.keys())'
+
+
+    def test_NETWORK_BY_P2PKH_PREFIX(self):
+        pass
