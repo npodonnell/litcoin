8bb1890b6d61fd39c793c4a2dc1c75d21fa464f8
odonnellnoel <odonnellnoel@github>
2019-03-31 11:11:27 +0100

    Added sighash type enum to txsighash.py

--- litcoin/txsighash.py
+++ litcoin/txsighash.py
@@ -2,12 +2,20 @@
 
 from litcoin.binhex import b
 
+# Sighash types
+# See script/interpreter.h
+SIGHASH_ALL = 1
+SIGHASH_NONE = 2
+SIGHASH_SINGLE = 3
+SIGHASH_ANYONECANPAY = 0x80
 
 HASH_ONE = b('0100000000000000000000000000000000000000000000000000000000000000')
 
-
+# src/test/sighash_tests.cpp
+# 
 def make_tx_sighash(script, tx, type, input_index):
     """
     Compute signature hash for a transaction input
     """
-    pass
+    if len(tx["inputs"]) <= input_index:
+        return HASH_ONE
