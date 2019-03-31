9ea210c4f237c024a56328fb5a0b3abf184fe910
odonnellnoel <odonnellnoel@github>
2019-03-31 21:15:54 +0100

    Work on txsighash

--- litcoin/txsighash.py
+++ litcoin/txsighash.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from litcoin.binhex import b
+from .tx import tx_copy
+from .script.compiler import compile_script
+from .script.operations import OP_CODESEPARATOR
+from .binhex import b
 
 # Sighash types
 # See script/interpreter.h
@@ -11,11 +14,33 @@ SIGHASH_ANYONECANPAY = 0x80
 
 HASH_ONE = b('0100000000000000000000000000000000000000000000000000000000000000')
 
-# src/test/sighash_tests.cpp
-# 
-def make_tx_sighash(script, tx, type, input_index):
+
+def find_and_delete(script, b):
+    """
+    TODO
+    """
+    return script
+
+
+def make_tx_sighash(script, tx, type, input_index, amount, sig_version, sighash_type):
     """
     Compute signature hash for a transaction input
+    Code pointers:
+      src/script/interpreter.cpp (new)
+      src/test/sighash_tests.cpp (old)
     """
     if len(tx["inputs"]) <= input_index:
         return HASH_ONE
+
+    # copy transaction to temporary copy
+    txtemp = tx_copy(tx)
+
+    # clear the unlocking scripts
+    for txinput in txtemp["inputs"]:
+        txinput["unlocking_script"] = compile_script([])
+    
+
+    txtemp["inputs"][input_index]["unlocking_script"] = find_and_delete(script, compile_script([OP_CODESEPERATOR]))
+
+    if (sighash_type & 0x1f) == SIGHASH_NONE:
+        txtemp["outputs"] = []
