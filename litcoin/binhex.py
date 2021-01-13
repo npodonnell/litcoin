5261545f1f6106e494c3f658b6661e8c981160cc
N. P. O'Donnell <6617714+npodonnell@users.noreply.github.com>
2021-01-13 22:16:08 +0000

    move tests

--- litcoin/binhex.py
+++ litcoin/binhex.py
@@ -7,7 +7,7 @@ def b(hex_str):
     """
     Convenience function to convert even-length hex string (excluding 0x) to bytes object
     """
-    assert type(hex_str) == str
+    assert type(hex_str) is str
     assert re.match('^([0-9a-fA-F]{2})*$', hex_str) is not None, 'Not a valid string of hex octets'
     return bytes.fromhex(hex_str)
 
@@ -16,5 +16,5 @@ def x(bytes_obj):
     """
     Convenience function to convert bytes object to even-length hex string (excluding 0x)
     """
-    assert type(bytes_obj) == bytes
+    assert type(bytes_obj) is bytes
     return bytes_obj.hex()
