bf3ce29cd73ec12f220664e8b1d0f24b4912625d
npodonnell <6617714+npodonnell@users.noreply.giDthub.com>
2021-02-10 21:50:03 +0000

    remove shebang, convert ' to "

--- litcoin/int32.py
+++ litcoin/int32.py
@@ -1,22 +1,20 @@
-#!/usr/bin/env python3
-
 from litcoin.serialization import ensure_enough_data
 
 INT32_SIZE_IN_BYTES = 4
 
 
 def validate_int32(n):
-    assert type(n) == int, 'type of `n` should be int'
-    assert -0x80000000 <= n <= 0x7fffffff, '`n` must fit within 32 bits'
+    assert type(n) == int, "type of `n` should be int"
+    assert -0x80000000 <= n <= 0x7fffffff, "`n` must fit within 32 bits"
 
 
 def serialize_int32(n):
     validate_int32(n)
-    return int.to_bytes(n, INT32_SIZE_IN_BYTES, byteorder='little', signed=True)
+    return int.to_bytes(n, INT32_SIZE_IN_BYTES, byteorder="little", signed=True)
 
 
 def deserialize_int32(data, pos=0):
     ensure_enough_data(data, pos, INT32_SIZE_IN_BYTES)
     next_pos = pos + INT32_SIZE_IN_BYTES
-    res = int.from_bytes(data[pos : next_pos], byteorder='little', signed=True)
-    return (res, next_pos)
+    res = int.from_bytes(data[pos : next_pos], byteorder="little", signed=True)
+    return res, next_pos
--- litcoin/int64.py
+++ litcoin/int64.py
@@ -1,22 +1,20 @@
-#!/usr/bin/env python3
-
 from litcoin.serialization import ensure_enough_data
 
 INT64_SIZE_IN_BYTES = 8
 
 
 def validate_int64(n):
-    assert type(n) == int, 'type of `n` should be int'
-    assert -0x8000000000000000 <= n <= 0x7fffffffffffffff, '`n` must fit within 64 bits'
+    assert type(n) == int, "type of `n` should be int"
+    assert -0x8000000000000000 <= n <= 0x7fffffffffffffff, "`n` must fit within 64 bits"
 
 
 def serialize_int64(n):
     validate_int64(n)
-    return int.to_bytes(n, INT64_SIZE_IN_BYTES, byteorder='little', signed=True)
+    return int.to_bytes(n, INT64_SIZE_IN_BYTES, byteorder="little", signed=True)
 
 
 def deserialize_int64(data, pos=0):
     ensure_enough_data(data, pos, INT64_SIZE_IN_BYTES)
     next_pos = pos + INT64_SIZE_IN_BYTES
-    res = int.from_bytes(data[pos : next_pos], byteorder='little', signed=True)
-    return (res, next_pos)
+    res = int.from_bytes(data[pos : next_pos], byteorder="little", signed=True)
+    return res, next_pos
